94820ca1d57559340956445010e9becc8f36bd6b
Ricardo Omar Lugo Vargas <ricardo.lugo@nuvem.mx>
2024-11-09 16:19:16 -0600

    main | Creando componente de Contacto y configurando router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ContactsComponent } from './contacts/contacts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: ContactsComponent},
+    {path: 'contacts', component: ContactsComponent},
+];
