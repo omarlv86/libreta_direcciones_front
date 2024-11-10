cdc0d2c8b0651c59ac4e3ec7b83087fdb9897356
Ricardo Omar Lugo Vargas <ricardo.lugo@nuvem.mx>
2024-11-10 00:40:12 -0600

    main | Creando formulario dinamico para guardar datos

--- src/app/create-contact/create-contact.component.ts
+++ src/app/create-contact/create-contact.component.ts
@@ -1,31 +1,50 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../api.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { FormGroup, FormControl, FormArray } from '@angular/forms';
-
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { FormGroup, FormControl, FormArray, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-create-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule, FormsModule],
   templateUrl: './create-contact.component.html',
   styleUrl: './create-contact.component.css'
 })
 export class CreateContactComponent {
 
   title:string = '';
-  formularioContacto!: FormGroup;
-  direcciones: any[] = [];
-  telefonos: any[] = [];
-  correos: any[] = [];
+  contactForm = new FormGroup({
+    name: new FormControl(''),
+    note: new FormControl(''),
+    birthday: new FormControl(''),
+    web: new FormControl(''),
+    work: new FormControl(''),
+    //address: new FormGroup([]),
+  });
+  addresses: any[] = [];
+  cellphones: any[] = [];
+  mails: any[] = [];
+
 
   constructor(private apiService: ApiService, private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
-      console.log('id: ',params.get('id'))
       this.title = params.get('id') == null ? 'Crear' : 'Actualizar'
     })
   }
+
+  addAddress() {
+    this.addresses.push({ calle: '', ciudad: '' });
+  }
+  addCellphone() {
+    this.cellphones.push({ phone: ''});
+  }
+  addMail() {
+    this.mails.push({ email: ''});
+  }
+
 
 }
