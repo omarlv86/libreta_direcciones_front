7af9f57a0db79ebac3ab3722c97fad4d84e24704
Ricardo Omar Lugo Vargas <ricardo.lugo@nuvem.mx>
2024-11-10 03:22:47 -0600

    main | Modificando formulario para que tenga direccion, mails, telefonos de manera dinamica

--- src/app/create-contact/create-contact.component.ts
+++ src/app/create-contact/create-contact.component.ts
@@ -1,50 +1,93 @@
-import { Component } from '@angular/core';
-import { ApiService } from '../api.service';
-import { ActivatedRoute, Router, RouterModule } from '@angular/router';
-import { FormGroup, FormControl, FormArray, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import {Component, inject} from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {Validators} from '@angular/forms';
+import {FormArray} from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-create-contact',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, RouterModule, FormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule, FormsModule,],
   templateUrl: './create-contact.component.html',
   styleUrl: './create-contact.component.css'
 })
 export class CreateContactComponent {
 
-  title:string = '';
-  contactForm = new FormGroup({
-    name: new FormControl(''),
-    note: new FormControl(''),
-    birthday: new FormControl(''),
-    web: new FormControl(''),
-    work: new FormControl(''),
-    //address: new FormGroup([]),
+  formBuilder = inject(FormBuilder);
+
+  profileForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    note: [''],
+    birthday: [''],
+    web: [''],
+    work: [''],
+    addresses: this.formBuilder.array([this.createAddress()]),
+    mails: this.formBuilder.array([this.createMail()]),
+    cellphones: this.formBuilder.array([this.createPhone()]),
   });
-  addresses: any[] = [];
-  cellphones: any[] = [];
-  mails: any[] = [];
 
+  constructor() {}
 
-  constructor(private apiService: ApiService, private router: Router, private route: ActivatedRoute) {}
-
-  ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      this.title = params.get('id') == null ? 'Crear' : 'Actualizar'
-    })
+  createAddress(): FormGroup {
+    return this.formBuilder.group({
+      street: [''],
+      state: [''],
+      city: [''],
+      country: ['']
+    });
   }
 
-  addAddress() {
-    this.addresses.push({ calle: '', ciudad: '' });
-  }
-  addCellphone() {
-    this.cellphones.push({ phone: ''});
-  }
-  addMail() {
-    this.mails.push({ email: ''});
+  get addresses(): FormArray {
+    return this.profileForm.get('addresses') as FormArray;
   }
 
+  addAddress(): void {
+    this.addresses.push(this.createAddress());
+  }
 
+  removeAddress(index: number): void {
+    this.addresses.removeAt(index);
+  }
+
+  createMail(): FormGroup {
+    return this.formBuilder.group({
+      email: [''],
+    });
+  }
+
+  get mails(): FormArray {
+    return this.profileForm.get('mails') as FormArray;
+  }
+
+  addMail(): void {
+    this.mails.push(this.createMail());
+  }
+
+  removeMail(index: number): void {
+    this.mails.removeAt(index);
+  }
+
+  createPhone(): FormGroup {
+    return this.formBuilder.group({
+      phone: [''],
+    });
+  }
+
+  get cellphones(): FormArray {
+    return this.profileForm.get('cellphones') as FormArray;
+  }
+
+  addPhone(): void {
+    this.cellphones.push(this.createPhone());
+  }
+
+  removePhone(index: number): void {
+    this.cellphones.removeAt(index);
+  }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+  }
 }
